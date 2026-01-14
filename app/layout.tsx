18386b0d57561f267dc4867d3381788ac244aabc
kingsley-sama <kngsley2018@gmail.com>
2026-01-14 08:33:58 +0100

    [feat]: color scheme and typography set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,107 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const epilogue = localFont({
+  src: [
+    {
+      path: "../public/fonts/Epilogue/static/Epilogue-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Epilogue/static/Epilogue-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Epilogue/static/Epilogue-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Epilogue/static/Epilogue-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Epilogue/static/Epilogue-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Epilogue/static/Epilogue-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Epilogue/static/Epilogue-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Epilogue/static/Epilogue-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Epilogue/static/Epilogue-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-epilogue",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const hankenGrotesk = localFont({
+  src: [
+    {
+      path: "../public/fonts/Hanken_Grotesk/static/HankenGrotesk-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Hanken_Grotesk/static/HankenGrotesk-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Hanken_Grotesk/static/HankenGrotesk-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Hanken_Grotesk/static/HankenGrotesk-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Hanken_Grotesk/static/HankenGrotesk-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Hanken_Grotesk/static/HankenGrotesk-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Hanken_Grotesk/static/HankenGrotesk-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Hanken_Grotesk/static/HankenGrotesk-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Hanken_Grotesk/static/HankenGrotesk-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-hanken-grotesk",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +117,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${epilogue.variable} ${hankenGrotesk.variable} antialiased`}
       >
         {children}
       </body>
